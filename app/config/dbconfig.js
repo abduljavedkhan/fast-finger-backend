0de2ba6ac89835ed498df669ef0b8ceefb27eca6
abduljavedkhan <abduljaved314@gmail.com>
2021-03-11 17:40:59 +0530

    error handling

--- app/config/dbconfig.js
+++ app/config/dbconfig.js
@@ -1,9 +1,5 @@
 module.exports = {
-    HOST: "freedb.tech",
-    USER: "freedbtech_javed",
-    PASSWORD: "freedb@21",
-    DB: "freedbtech_fastfingertest",
-    dialect: "mysql",
+
     pool: {
       max: 10,
       min: 0,
