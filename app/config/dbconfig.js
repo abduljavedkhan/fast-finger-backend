6e73dd6734301db07e21f5b936bf5c56437a06bc
abduljavedkhan <abduljaved314@gmail.com>
2021-03-11 17:52:18 +0530

    env variables handling

--- app/config/dbconfig.js
+++ app/config/dbconfig.js
@@ -1,5 +1,9 @@
 module.exports = {
-
+  HOST: process.env.DB_URL,
+  USER: process.env.DBUSER,
+  PASSWORD: process.env.DBPWD,
+  DB: process.env.DBNAME,
+  dialect: process.env.DIALECT, 
     pool: {
       max: 10,
       min: 0,
